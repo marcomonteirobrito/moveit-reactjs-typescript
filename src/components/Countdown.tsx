31d6d810d9336fd7fc01b9eab97d0c04ae1007fe
Marco Monteiro <marco_monteiro@live.com>
2021-03-16 22:31:52 -0300

    Alterações finais do projeto

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,32 +1,19 @@
 import { useState, useEffect, useContext } from 'react';
 
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/Countdown.module.css';
 
-let countdownTimeout: NodeJS.Timeout;
-
 export function Countdown() {
-    const [time, setTime] = useState(0.1 * 60);
-    const [isActive, setIsActive] = useState(false);
-    const [hasFinished, setHasFinished] = useState(false);
-
-    const { startNewChallenge } = useContext(ChallengesContext);
-
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-
-    useEffect(() => {
-        if (isActive && time > 0) {
-            countdownTimeout = setTimeout(() => {
-                setTime(time - 1);
-            }, 1000);
-        } else if (isActive && time === 0) {
-            setIsActive(false);
-            setHasFinished(true);
-            startNewChallenge();
-        }
-    }, [isActive, time]);
+    const {
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        startCountdown,
+        resetCountdown,
+    } = useContext(CountdownContext);
 
     const [minuteLeft, minuteRight] = String(minutes)
         .padStart(2, '0')
@@ -34,16 +21,6 @@ export function Countdown() {
     const [secondLeft, secondRight] = String(seconds)
         .padStart(2, '0')
         .split('');
-
-    const startCountdown = () => {
-        setIsActive(true);
-    };
-
-    const resetCountdown = () => {
-        clearTimeout(countdownTimeout);
-        setIsActive(false);
-        setTime(0.1 * 60);
-    };
 
     return (
         <div>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import challenges from '../../challenges.json';
+import { LevelUpModal } from '../components/LevelUpModal';
 
 interface Challenge {
     type: 'body' | 'eye';
@@ -15,6 +16,8 @@ interface ChallengesContextData {
     levelUp: () => void;
     startNewChallenge: () => void;
     resetChallenge: () => void;
+    completeChallenge: () => void;
+    closeLevelUpModal: () => void;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
@@ -24,24 +27,87 @@ export function ChallengesProvider({ children }) {
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
     const [activeChallenge, setActiveChallenge] = useState(null);
+    const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false);
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+    useEffect(() => {
+        Notification.requestPermission();
+    }, []);
+
+    useEffect(() => {
+        const levelStorage = Number(localStorage.getItem('level')) || 1;
+        const currentExperienceStorage =
+            Number(localStorage.getItem('currentExperience')) || 0;
+        const challengesCompletedStorage =
+            Number(localStorage.getItem('challengesCompleted')) || 0;
+
+        setLevel(levelStorage);
+        setCurrentExperience(currentExperienceStorage);
+        setChallengesCompleted(challengesCompletedStorage);
+    }, []);
+
+    useEffect(() => {
+        saveLocalStorage();
+    }, [level, currentExperience, challengesCompleted]);
+
     function levelUp() {
         setLevel(level + 1);
+        setIsLevelUpModalOpen(true);
+    }
+
+    function closeLevelUpModal() {
+        setIsLevelUpModalOpen(false);
     }
 
     function startNewChallenge() {
         const randomChallengesIndex = Math.floor(
             Math.random() * challenges.length
         );
+
         const challenge = challenges[randomChallengesIndex];
 
         setActiveChallenge(challenge);
+
+        new Audio('/notification.mp3').play();
+
+        if (Notification.permission === 'granted') {
+            new Notification('Novo desafio', {
+                body: `Valendo ${challenge.amount}xp`,
+            });
+        }
     }
 
     function resetChallenge() {
         setActiveChallenge(null);
+    }
+
+    function completeChallenge() {
+        if (!activeChallenge) {
+            return;
+        }
+
+        const { amount } = activeChallenge;
+
+        let finalExperience = currentExperience + amount;
+
+        if (finalExperience >= experienceToNextLevel) {
+            finalExperience = finalExperience - experienceToNextLevel;
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperience);
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1);
+    }
+
+    function saveLocalStorage() {
+        localStorage.setItem('level', String(level));
+        localStorage.setItem('currentExperience', String(currentExperience));
+        localStorage.setItem(
+            'challengesCompleted',
+            String(challengesCompleted)
+        );
     }
 
     return (
@@ -55,9 +121,13 @@ export function ChallengesProvider({ children }) {
                 levelUp,
                 startNewChallenge,
                 resetChallenge,
+                completeChallenge,
+                closeLevelUpModal,
             }}
         >
             {children}
+
+            {isLevelUpModalOpen && <LevelUpModal />}
         </ChallengesContext.Provider>
     );
 }
